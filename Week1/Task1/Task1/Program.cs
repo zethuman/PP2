1f11ba9865c8145546903c41d44701ce9f7ae08c
Rakhat <zhumanrakhat01@gmail.com>
2019-01-26 19:57:14 +0600

    nemnojko izmenil

--- Week1/Task1/Task1/Program.cs
+++ Week1/Task1/Task1/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
             List<int> Prime = new List<int>();  // Создал список чтобы легче было хранить данные и посчитать сколько их
@@ -21,11 +19,10 @@ namespace Program
             }
             for (int i = 0; i < arr.Length; i++)
             {
-                if (PrimeNum(arr[i]) == 1) // передал числа из массива в метод PrimeNum 
+                if (IsPrime(arr[i]) == 1) // передал числа из массива в метод IsPrime 
                 {
                     Prime.Add(arr[i]); // если наш метод равен 1, то добавляем в список(List) Prime
                 }
-                
             }
 
             Console.WriteLine(Prime.Count); // посчитываю количество простых чисел и вывожу
@@ -37,11 +34,10 @@ namespace Program
             Console.ReadLine();
         }
 
-        public static int PrimeNum(int num)
+        public static int IsPrime(int num)
         {
             for (int i = 2; i <= num; i++) // перебираем числа от 2 до число которую передали
             { 
-                
                 if ((i == num) || (i > Math.Sqrt(num))) // проверяем если i равно числу которую мы передали (num) то возвращаем true или 1  
                 {                                       // например если 2 = 2 то возвращаем true или 1
                     return 1;                           // нам достаточно проверить до корня числа по Решето Эратосфена
