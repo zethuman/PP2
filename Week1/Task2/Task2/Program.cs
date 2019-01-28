5fd8134a652500d40d609266602c30224db477cb
Rakhat <zhumanrakhat01@gmail.com>
2019-01-28 17:36:28 +0600

    zakonchil task 2

--- Week1/Task2/Task2/Program.cs
+++ Week1/Task2/Task2/Program.cs
@@ -2,15 +2,15 @@
 
 namespace Program
 {
-    class Student
+    class Student // создаю класс 
     {
-        public string name;
-        public string id;
+        public string name; 
+        public string id;  // идентифицироваю
 
         public Student(string name, string id)
         {
-            this.name = name;
-            this.id = id;
+            this.name = name; 
+            this.id = id;  // обеспечиваю доступ к текущему экземпляру класса с помощью ключевого слова this
         }
     }
 
@@ -18,12 +18,12 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Student st1 = new Student(Console.ReadLine(),Console.ReadLine());
+            Student st1 = new Student(Console.ReadLine(),Console.ReadLine()); // считываю с консоли name, потом id и передаю в класс
 
-            int year = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());  
             
-            Console.Write(st1.name + " " + st1.id + " " + ++year);
-            Console.ReadLine();
+            Console.Write(st1.name + " " + st1.id + " " + ++year); // вывожу с инкрементом year
+            Console.ReadKey();
         }
     }
 }
