0384ea013138f374f8a36c1eae9126115ae28d18
Rakhat <zhumanrakhat01@gmail.com>
2019-02-02 11:28:55 +0600

    sozdal week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("../../input.txt"); 
+            string s = sr.ReadToEnd(); // считал строку из инпута 
+          
+            int n = s.Length; // присваевал к n чтобы было легче
+
+            for (int i = 0; i < n / 2; i++) // пробегался до половины строки 
+            {
+                if (s[i] != s[n - i - 1]) // если первая половина не равна на вторую 
+                {
+                    Console.WriteLine("No"); // вывожу No
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            Console.WriteLine("Yes"); // иначе Yes
+            Console.ReadKey();
+        }
+    }
+    
+}
