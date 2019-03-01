7b112c4dcd60b962842e7f4081b468ca10cc3937
Rakhat <zhumanrakhat01@gmail.com>
2019-03-01 21:09:25 +0600

    Re:

---
+++ Week6/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(75,75);
+            Console.SetBufferSize(75,75);
+            Console.ReadKey();
+        }
+    }
+}
